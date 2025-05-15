85d7103cf9aac8cd6ecc8059447a1dba6f57aeb2
Besomi22 <sbelllali000@gmail.com>
2025-05-15 11:07:47 +0100

    Adding 1-index/html file

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
